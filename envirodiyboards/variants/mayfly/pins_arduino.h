200cf74aee0a7632cc674c8cfa0e600978943dae
Sara Damiano <sdamiano@stroudcenter.org>
2021-05-18 12:48:36 -0400

    Update package size
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- envirodiyboards/variants/mayfly/pins_arduino.h
+++ envirodiyboards/variants/mayfly/pins_arduino.h
@@ -48,8 +48,9 @@
 #define NUM_ANALOG_INPUTS           8
 
 #define analogInputToDigitalPin(p)  ((p < NUM_ANALOG_INPUTS) ? (p) + 24 : -1)
+#define digitalPinToInterrupt(p)  ((p) == 10 ? 2 : ((p) == 2 ? 0 : ((p) == 3 ? 1 : NOT_AN_INTERRUPT)))
 
-#define digitalPinHasPWM(p)         ((p) == 3 || (p) == 4 || (p) == 6 || (p) == 7 || (p) == 12 || (p) == 13 || (p) == 14 || (p) == 15)
+#define digitalPinHasPWM(p)         ((p) == 11 || (p) == 12 || (p) == 14 || (p) == 15 || (p) == 4 || (p) == 5 || (p) == 6 || (p) == 7)
 
 static const uint8_t SS   = 12;
 static const uint8_t MOSI = 13;
@@ -60,8 +61,8 @@ static const uint8_t SDA = 17;
 static const uint8_t SCL = 16;
 
 static const uint8_t GROVEPWR = 22;
-static const uint8_t GROVEPWR_OFF = LOW;
-static const uint8_t GROVEPWR_ON = HIGH;
+static const uint8_t GROVEPWR_OFF = 0;
+static const uint8_t GROVEPWR_ON = 1;
 
 static const uint8_t BEEDTR = 23;
 static const uint8_t BEERTS = 20;
@@ -84,14 +85,14 @@ static const uint8_t A6 = 30;
 static const uint8_t A7 = 31;
 
 /*
-   PCINT31-24: D7-0   : bit 3
-   PCINT15-8:  D15-8  : bit 1
-   PCINT23-16: D23-16 : bit 2
-   PCINT7-0:   D31-24 : bit 0 (also A0..A7)
+   PCINT31-24: D7-0   : bit 3(D)
+   PCINT15-8:  D15-8  : bit 1(B)
+   PCINT23-16: D23-16 : bit 2(C)
+   PCINT7-0:   D31-24 : bit 0(A) (also A0..A7)
 */
 
 #define digitalPinToPCICR(p)    (((p) >= 0 && (p) < NUM_DIGITAL_PINS) ? (&PCICR) : ((uint8_t *)0))
-#define digitalPinToPCICRbit(p) (((p) <= 7) ? 3         : (((p) <= 15) ? 1         : (((p) <= 23) ? 2         : 0)))
+#define digitalPinToPCICRbit(p) (((p) <= 7) ? 3         : (((p) <= 15) ? 1         : (((p) <= 23) ? 2         : 0        )))
 #define digitalPinToPCMSK(p)    (((p) <= 7) ? (&PCMSK3) : (((p) <= 15) ? (&PCMSK1) : (((p) <= 23) ? (&PCMSK2) : (&PCMSK0))))
 #define digitalPinToPCMSKbit(p) ((p) % 8)
 
@@ -108,137 +109,137 @@ static const uint8_t A7 = 31;
 // and writing)
 const uint16_t PROGMEM port_to_mode_PGM[] =
 {
-	NOT_A_PORT,
-	(uint16_t) &DDRA,
-	(uint16_t) &DDRB,
-	(uint16_t) &DDRC,
-	(uint16_t) &DDRD,
+    NOT_A_PORT,
+    (uint16_t) &DDRA,
+    (uint16_t) &DDRB,
+    (uint16_t) &DDRC,
+    (uint16_t) &DDRD,
 };
 
 const uint16_t PROGMEM port_to_output_PGM[] =
 {
-	NOT_A_PORT,
-	(uint16_t) &PORTA,
-	(uint16_t) &PORTB,
-	(uint16_t) &PORTC,
-	(uint16_t) &PORTD,
+    NOT_A_PORT,
+    (uint16_t) &PORTA,
+    (uint16_t) &PORTB,
+    (uint16_t) &PORTC,
+    (uint16_t) &PORTD,
 };
 
 const uint16_t PROGMEM port_to_input_PGM[] =
 {
-	NOT_A_PORT,
-	(uint16_t) &PINA,
-	(uint16_t) &PINB,
-	(uint16_t) &PINC,
-	(uint16_t) &PIND,
+    NOT_A_PORT,
+    (uint16_t) &PINA,
+    (uint16_t) &PINB,
+    (uint16_t) &PINC,
+    (uint16_t) &PIND,
 };
 
 const uint8_t PROGMEM digital_pin_to_port_PGM[] =
 {
-	PD, /* 0 */
-	PD,
-	PD,
-	PD,
-	PD,
-	PD,
-	PD,
-	PD,
-	PB, /* 8 */
-	PB,
-	PB,
-	PB,
-	PB,
-	PB,
-	PB,
-	PB,
-	PC, /* 16 */
-	PC,
-	PC,
-	PC,
-	PC,
-	PC,
-   	PC,
-	PC,
-	PA, /* 24 */
-	PA,
-	PA,
-	PA,
-	PA,
-	PA,
-	PA,
-	PA  /* 31 */
+    PD, /* D0 */
+    PD,
+    PD,
+    PD,
+    PD,
+    PD,
+    PD,
+    PD,
+    PB, /* D8 */
+    PB,
+    PB,
+    PB,
+    PB,
+    PB,
+    PB,
+    PB,
+    PC, /* D16 */
+    PC,
+    PC,
+    PC,
+    PC,
+    PC,
+    PC,
+    PC,
+    PA, /* D24 */
+    PA,
+    PA,
+    PA,
+    PA,
+    PA,
+    PA,
+    PA  /* D31 */
 };
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] =
 {
-	_BV(0), /* 0, port D */
-	_BV(1),
-	_BV(2),
-	_BV(3),
-	_BV(4),
-	_BV(5),
-	_BV(6),
-	_BV(7),
-	_BV(0), /* 8, port B */
-	_BV(1),
-	_BV(2),
-	_BV(3),
-	_BV(4),
-	_BV(5),
-	_BV(6),
-	_BV(7),
-	_BV(0), /* 16, port C */
-	_BV(1),
-	_BV(2),
-	_BV(3),
-	_BV(4),
-	_BV(5),
-	_BV(6),
-	_BV(7),
-	_BV(0), /* 24, port A */
-	_BV(1),
-	_BV(2),
-	_BV(3),
-	_BV(4),
-	_BV(5),
-	_BV(6),
-	_BV(7)
+    _BV(0), /* D0, port D */
+    _BV(1),
+    _BV(2),
+    _BV(3),
+    _BV(4),
+    _BV(5),
+    _BV(6),
+    _BV(7),
+    _BV(0), /* D8, port B */
+    _BV(1),
+    _BV(2),
+    _BV(3),
+    _BV(4),
+    _BV(5),
+    _BV(6),
+    _BV(7),
+    _BV(0), /* D16, port C */
+    _BV(1),
+    _BV(2),
+    _BV(3),
+    _BV(4),
+    _BV(5),
+    _BV(6),
+    _BV(7),
+    _BV(0), /* D24, port A */
+    _BV(1),
+    _BV(2),
+    _BV(3),
+    _BV(4),
+    _BV(5),
+    _BV(6),
+    _BV(7)
 };
 
 const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
 {
-	NOT_ON_TIMER, 	/* 0  - PD0 */
-	NOT_ON_TIMER, 	/* 1  - PD1 */
-	NOT_ON_TIMER, 	/* 2  - PD2 */
-	NOT_ON_TIMER, 	/* 3  - PD3 */
-	TIMER1B,     	/* 4  - PD4 */
-	TIMER1A,     	/* 5  - PD5 */
-	TIMER2B,     	/* 6  - PD6 */
-	TIMER2A,     	/* 7  - PD7 */
-	NOT_ON_TIMER, 	/* 8  - PB0 */
-	NOT_ON_TIMER, 	/* 9  - PB1 */
-	NOT_ON_TIMER, 	/* 10 - PB2 */
-	TIMER0A,     	/* 11 - PB3 */
-	TIMER0B, 	/* 12 - PB4 */
-	NOT_ON_TIMER, 	/* 13 - PB5 */
-	TIMER3A, 	/* 14 - PB6 */
-	TIMER3B,	/* 15 - PB7 */
-	NOT_ON_TIMER, 	/* 16 - PC0 */
-	NOT_ON_TIMER,   /* 17 - PC1 */
-	NOT_ON_TIMER,   /* 18 - PC2 */
-	NOT_ON_TIMER,   /* 19 - PC3 */
-	NOT_ON_TIMER,   /* 20 - PC4 */
-	NOT_ON_TIMER,   /* 21 - PC5 */
-	NOT_ON_TIMER,   /* 22 - PC6 */
-	NOT_ON_TIMER,   /* 23 - PC7 */
-	NOT_ON_TIMER,   /* 24 - PA0 */
-	NOT_ON_TIMER,   /* 25 - PA1 */
-	NOT_ON_TIMER,   /* 26 - PA2 */
-	NOT_ON_TIMER,   /* 27 - PA3 */
-	NOT_ON_TIMER,   /* 28 - PA4 */
-	NOT_ON_TIMER,   /* 29 - PA5 */
-	NOT_ON_TIMER,   /* 30 - PA6 */
-	NOT_ON_TIMER   /* 31 - PA7 */
+    NOT_ON_TIMER,  /* D0  - PD0 */
+    NOT_ON_TIMER,  /* D1  - PD1 */
+    NOT_ON_TIMER,  /* D2  - PD2 */
+    NOT_ON_TIMER,  /* D3  - PD3 */
+    TIMER1B,       /* D4  - PD4 */
+    TIMER1A,       /* D5  - PD5 */
+    TIMER2B,       /* D6  - PD6 */
+    TIMER2A,       /* D7  - PD7 */
+    NOT_ON_TIMER,  /* D8  - PB0 */
+    NOT_ON_TIMER,  /* D9  - PB1 */
+    NOT_ON_TIMER,  /* D10 - PB2 */
+    TIMER0A,       /* D11 - PB3 */
+    TIMER0B,       /* D12 - PB4 */
+    NOT_ON_TIMER,  /* D13 - PB5 */
+    TIMER3A,       /* D14 - PB6 */
+    TIMER3B,       /* D15 - PB7 */
+    NOT_ON_TIMER,  /* D16 - PC0 */
+    NOT_ON_TIMER,  /* D17 - PC1 */
+    NOT_ON_TIMER,  /* D18 - PC2 */
+    NOT_ON_TIMER,  /* D19 - PC3 */
+    NOT_ON_TIMER,  /* D20 - PC4 */
+    NOT_ON_TIMER,  /* D21 - PC5 */
+    NOT_ON_TIMER,  /* D22 - PC6 */
+    NOT_ON_TIMER,  /* D23 - PC7 */
+    NOT_ON_TIMER,  /* D24 - PA0 */
+    NOT_ON_TIMER,  /* D25 - PA1 */
+    NOT_ON_TIMER,  /* D26 - PA2 */
+    NOT_ON_TIMER,  /* D27 - PA3 */
+    NOT_ON_TIMER,  /* D28 - PA4 */
+    NOT_ON_TIMER,  /* D29 - PA5 */
+    NOT_ON_TIMER,  /* D30 - PA6 */
+    NOT_ON_TIMER   /* D31 - PA7 */
 };
 
 #endif // ARDUINO_MAIN
